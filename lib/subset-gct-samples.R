0f5ba1fbd17e6fd89e304af56b081006f4f9bab9
Gil Tomas <gil.tms@gmail.com>
2014-11-20 15:58:56 +0100

    Write /subset-gct-samples.R

---
+++ lib/subset-gct-samples.R
@@ -0,0 +1,10 @@
+## subsetGCTsamples
+## subsets gct's samples with a logical vector
+
+subsetGCTsamples <- function (gct,
+                              log.vec) { # logical vector with length
+                                        # equal to the number of
+                                        # samples in the gct
+    list (row.descriptions = gct$row.descriptions,
+          data = gct$data[, log.vec])
+}
