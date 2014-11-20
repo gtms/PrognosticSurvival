fa547dc2ccb30d0c770ee9fedefd8b81c77a9ba1
Gil Tomas <gil.tms@gmail.com>
2014-11-20 17:04:14 +0100

    Write lib/loop-sigs-list-gct.R

---
+++ lib/loop-sigs-list-gct.R
@@ -0,0 +1,29 @@
+## loopSigsListGCT
+
+## this is an internal of testMSigDBgct
+
+loopSigsListGCT <- function (gct,
+                             surv,
+                             sig.lst,
+                             pca.method = "svd",
+                             minGenes = 5,
+                             doParallel = FALSE,
+                             scale.matrix = FALSE) {
+    ## only considers gene signatures with at least 'minGenes' present in the
+    ## expression matrix
+    isNA <- laply (sig.lst, function (sig) {
+        length (intersect (sig, gct$row.descriptions)) < minGenes
+    },
+                   .parallel = doParallel)
+    cox.dfr <- ldply (sig.lst[!isNA], function (sig) {  
+        score <- binaryPC1Score (gct,
+                                 sig,
+                                 method = pca.method,
+                                 scale.matrix = scale.matrix)
+        data.frame (logRankTest (score$discrete,
+                                 surv[!score$rm.samples]))
+    },
+                      .parallel = doParallel)
+    names (cox.dfr)[1] <- "signature"
+    cox.dfr
+}
