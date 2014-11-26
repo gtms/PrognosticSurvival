6083c6934672ee320db3e30e7efa329d76ff07b6
Gil Tomas <gil.tms@gmail.com>
2014-11-26 17:21:14 +0100

    Write lib/install-packages.R

---
+++ lib/install-packages.R
@@ -0,0 +1,33 @@
+## This script installs all required packages for analysis
+## Gil Tomás, IRIBHM, ULB
+## <gil.tomas@ulb.ac.be>
+
+## install Bioconductor
+source ("http://bioconductor.org/biocLite.R")
+biocLite ()
+
+## install CRAN dependencies for MicroarrayToolbox
+cran.pkgs <- c ("data.table", "MASS", "ggplot2", "RColorBrewer", "samr", "devtools", "ProjectTemplate")
+lapply (cran.pkgs, install.packages, repos = "http://cran.r-project.org")
+
+## install Bioconductor dependencies for MicroarrayToolbox
+bioc.pkgs <- c ("annotate", "org.Hs.eg.db", "GO.db")
+source ("http://bioconductor.org/biocLite.R")
+lapply (bioc.pkgs, biocLite)
+
+## install MicroarrayToolbox
+library (devtools)
+install_github ("gtms/MicroarrayToolbox")
+
+## load MicroarrayToolbox and install remaining packages
+library (MicroarrayToolbox)
+pkgs.dfr <- data.frame (pkg = c ("impute", "pcaMethods", "impute", "qvalue", "SNAGEE",
+                            "amap", "survival", "doMC", "MASS", "stargazer"),
+                        repos = c (rep ("bioc", 5), rep ("cran", 5)),
+                        stringsAsFactors = FALSE)
+instant.pkgs (pkgs.dfr)
+
+## quit
+sessionInfo ()
+q (save = "no")
+
