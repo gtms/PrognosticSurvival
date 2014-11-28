08681501f795ac0bb144429db71de1bfaffc610f
Gil Tomas <gil.tms@gmail.com>
2014-11-28 15:05:28 +0100

    Reorder commands in lib/install-packages.R script to comply with package dependencies

--- lib/install-packages.R
+++ lib/install-packages.R
@@ -6,14 +6,14 @@
 source ("http://bioconductor.org/biocLite.R")
 biocLite ()
 
+## install Bioconductor dependencies for MicroarrayToolbox
+bioc.pkgs <- c ("annotate", "org.Hs.eg.db", "GO.db", "impute")
+source ("http://bioconductor.org/biocLite.R")
+lapply (bioc.pkgs, biocLite)
+
 ## install CRAN dependencies for MicroarrayToolbox
 cran.pkgs <- c ("data.table", "MASS", "ggplot2", "RColorBrewer", "samr", "devtools", "ProjectTemplate")
 lapply (cran.pkgs, install.packages, repos = "http://cran.r-project.org")
-
-## install Bioconductor dependencies for MicroarrayToolbox
-bioc.pkgs <- c ("annotate", "org.Hs.eg.db", "GO.db")
-source ("http://bioconductor.org/biocLite.R")
-lapply (bioc.pkgs, biocLite)
 
 ## install MicroarrayToolbox
 library (devtools)
@@ -21,9 +21,9 @@ install_github ("gtms/MicroarrayToolbox")
 
 ## load MicroarrayToolbox and install remaining packages
 library (MicroarrayToolbox)
-pkgs.dfr <- data.frame (pkg = c ("impute", "pcaMethods", "impute", "qvalue", "SNAGEE",
+pkgs.dfr <- data.frame (pkg = c ("pcaMethods", "qvalue", "SNAGEE",
                             "amap", "survival", "doMC", "MASS", "stargazer"),
-                        repos = c (rep ("bioc", 5), rep ("cran", 5)),
+                        repos = c (rep ("bioc", 3), rep ("cran", 5)),
                         stringsAsFactors = FALSE)
 instant.pkgs (pkgs.dfr)
 
