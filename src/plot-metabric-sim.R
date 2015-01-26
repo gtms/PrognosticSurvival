3170302e8d4c8f9de1b309a7ccd422c5ee4793db
Gil Tomas <gil.tms@gmail.com>
2015-01-26 13:57:24 +0100

    Updates code of src/plot-metabric-sim.R

--- src/plot-metabric-sim.R
+++ src/plot-metabric-sim.R
@@ -20,6 +20,7 @@ myBoxplot <- function (data,
                        xat = NULL,
                        xlab = "",
                        ylab = "",
+                       x.axis = NULL,
                        main = "",
                        file = "",
                        col = "black", ...) {
@@ -27,11 +28,22 @@ myBoxplot <- function (data,
         do.call (gsub (".*([a-z]+{3})$", "\\1", file), list (file))
         par (mar = c (6.1, 6.5, 4.1, 1.1))
     }
-    par (mar = c (6.1, 6.5, 4.1, 1.1))
+    if (!is.null (x.axis)) {
+        my.xaxt <- "n"
+    } else {
+        my.xaxt <- NULL
+        axis (1,
+              at = seq_along (x.axis),
+              labels = x.axis,
+              cex.axis = 2.5,
+              las = 2)
+    }
+    ## par (mar = c (6.1, 6.5, 4.1, 1.1))
     boxplot (data,
              outline = FALSE,
              frame = TRUE,
              lwd = 1,
+             xaxt = my.xaxt,
              cex.axis = 2.5,
              cex.lab = 2.5, ...)
     stripchart (as.data.frame (data),
@@ -39,7 +51,7 @@ myBoxplot <- function (data,
                 method = "jitter",
                 pch = 19,
                 vertical = TRUE,
-                ## cex = 2,
+                cex = 2,
                 col = transparent.rgb (col))
     mtext (side = 1,
            text = xlab,
@@ -57,6 +69,7 @@ myBoxplot <- function (data,
 
 ## defines script-specific variables
 frac.label <- "% outcome-associated genes"
+write.path <- "graphs/metabric-sims"
 
 ## * Preprocess
 ## multiply all values by 100 to yield percentages
@@ -70,7 +83,8 @@ plt.lst[["sampling.variability"]] <- unlist (plt.lst[["sampling.variability"]])
 
 ## * Plot
 ## defines layout of device; initializes device
-svg (file = "graphs/metabric-sims.svg")
+svg (file = file.path (write.path, "metabric-sims.svg"),
+     width = 14, height = 10)
 par (mar = c (6.1, 6.5, 4.1, 1.1))
 
 ## sets layout (2 x 3)
@@ -81,62 +95,141 @@ layout (mat = matrix (1:6, ncol = 3, byrow = TRUE))
 hist (plt.lst[["sampling.variability"]],
       xlab = frac.label,
       main = "",
-cex.lab = 2.5,
-cex.axis = 2.5)
+      mgp = c (3, 1, 0),
+      cex.lab = 2.5,
+      cex.axis = 2.5)
 
 ## panel B
 ## cohort.size
-myBoxplot (plt.lst[["cohort.size"]],
-           xlab = "Number of patients",
-           ylab = frac.label,
-           xaxt = "n")
+## myBoxplot (plt.lst[["cohort.size"]],
+##            xlab = "Number of patients",
+##            ylab = frac.label,
+##            xaxt = "n")
+x.axis <- as.numeric (names (plt.lst[["cohort.size"]])) / 100
+boxplot (plt.lst[["cohort.size"]],
+         outline = FALSE,
+         frame = TRUE,
+         lwd = 1,
+         xaxt = "n",
+         xlab = "Number of patients (x100)",
+         ylab = frac.label,
+         cex.axis = 2.5,
+         cex.lab = 2.5)
+stripchart (as.data.frame (plt.lst[["cohort.size"]]),
+            add = TRUE,
+            method = "jitter",
+            pch = 19,
+            vertical = TRUE,
+            cex = 2,
+            col = transparent.rgb ("black"))
+axis (1,
+      at = seq_along (x.axis),
+      labels = x.axis,
+      cex.axis = 2.5,
+      mgp = c (5, 1, 0),
+      las = 2)
+
 ## file = "boxplot-metabric-N.svg")
 ## plot sample size (on top, first 4)
-sample.size.top <- as.numeric (names (plt.lst[["cohort.size"]][1:4]))
-text (x = 1:4 - .15,
-      y = max (do.call (c, plt.lst[["cohort.size"]])) - 2,
-      labels = sprintf ("n = %d", sample.size.top),
-      srt = 90, # 90 degrees
-      pos = 1) # right justififed
-sample.size.bot <- as.numeric (names (plt.lst[["cohort.size"]][5:9]))
-text (x = 5:9 - .15,
-      y = min (do.call (c, plt.lst[["cohort.size"]])) + 1,
-      labels = sprintf ("n = %d", sample.size.bot),
-      srt = 90, # 90 degrees
-      pos = 4) # left justififed
+## sample.size.top <- as.numeric (names (plt.lst[["cohort.size"]][1:4]))
+## text (x = 1:4 - .15,
+##       y = max (do.call (c, plt.lst[["cohort.size"]])) - 2,
+##       labels = sprintf ("n = %d", sample.size.top),
+##       srt = 90, # 90 degrees
+##       pos = 1) # right justififed
+## sample.size.bot <- as.numeric (names (plt.lst[["cohort.size"]][5:9]))
+## text (x = 5:9 - .15,
+##       y = min (do.call (c, plt.lst[["cohort.size"]])) + 1,
+##       labels = sprintf ("n = %d", sample.size.bot),
+##       srt = 90, # 90 degrees
+##       pos = 4) # left justififed
 
 ## panel C
 ## follow.up.time
-myBoxplot (plt.lst[["follow.up.time"]],
-           xlab = "Maximum follow-up (years)",
-           ylab = frac.label,
-           xaxt = "n")
+x.axis <- round (as.numeric (names (plt.lst[["follow.up.time"]])) / 12, 1)
+boxplot (plt.lst[["follow.up.time"]],
+         outline = FALSE,
+         frame = TRUE,
+         lwd = 1,
+         xaxt = "n",
+         xlab = "follow-up time (years)",
+         ylab = frac.label,
+         cex.axis = 2.5,
+         cex.lab = 2.5)
+stripchart (as.data.frame (plt.lst[["follow.up.time"]]),
+            add = TRUE,
+            method = "jitter",
+            pch = 19,
+            vertical = TRUE,
+            cex = 2,
+            col = transparent.rgb ("black"))
+axis (1,
+      at = seq_along (x.axis),
+      labels = x.axis,
+      cex.axis = 2.5,
+      mgp = c (5, 1, 0),
+      las = 2)
+
+## myBoxplot (plt.lst[["follow.up.time"]],
+##            xlab = "Maximum follow-up (years)",
+##            ylab = frac.label,
+##            xaxt = "n")
 ## file = "boxplot-metabric-follow-up.svg")
-fut.top <- as.numeric (names (plt.lst[["follow.up.time"]][1])) / 12
-text (x = 1 - .15,
-      y = max (do.call (c, plt.lst[["follow.up.time"]])) - 2,
-      labels = sprintf ("%.1f yrs", fut.top),
-      srt = 90, # 90 degrees
-      pos = 1) # right justififed
-fut.bot <- as.numeric (names (plt.lst[["follow.up.time"]][2:9])) / 12
-text (x = 2:9 - .15,
-      y = min (do.call (c, plt.lst[["follow.up.time"]])),
-      labels = sprintf ("%.1f yrs", fut.bot),
-      srt = 90, # 90 degrees
-      pos = 4) # left justififed
+## fut.top <- as.numeric (names (plt.lst[["follow.up.time"]][1])) / 12
+## text (x = 1 - .15,
+##       y = max (do.call (c, plt.lst[["follow.up.time"]])) - 2,
+##       labels = sprintf ("%.1f yrs", fut.top),
+##       srt = 90, # 90 degrees
+##       pos = 1) # right justififed
+## fut.bot <- as.numeric (names (plt.lst[["follow.up.time"]][2:9])) / 12
+## text (x = 2:9 - .15,
+##       y = min (do.call (c, plt.lst[["follow.up.time"]])),
+##       labels = sprintf ("%.1f yrs", fut.bot),
+##       srt = 90, # 90 degrees
+##       pos = 4) # left justififed
 
 ## panel D
 ## er.status
-myBoxplot (plt.lst[["er.status"]],
-           xlab = "% ER+ tumors",
-           ylab = frac.label)
+boxplot (plt.lst[["er.status"]],
+         outline = FALSE,
+         frame = TRUE,
+         lwd = 1,
+         xlab = "% ER+ tumors",
+         ylab = frac.label,
+         cex.axis = 2.5,
+         cex.lab = 2.5)
+stripchart (as.data.frame (plt.lst[["er.status"]]),
+            add = TRUE,
+            method = "jitter",
+            pch = 19,
+            vertical = TRUE,
+            cex = 2,
+            col = transparent.rgb ("black"))
+## myBoxplot (plt.lst[["er.status"]],
+##            xlab = "% ER+ tumors",
+##            ylab = frac.label)
 ## file = "boxplot-metabric-er.svg")
 
 ## panel E
 ## nodal.status
-myBoxplot (plt.lst[["nodal.status"]],
-           xlab = "% node positive patients",
-           ylab = frac.label)
+boxplot (plt.lst[["nodal.status"]],
+         outline = FALSE,
+         frame = TRUE,
+         lwd = 1,
+         xlab = "% node positive patients",
+         ylab = frac.label,
+         cex.axis = 2.5,
+         cex.lab = 2.5)
+stripchart (as.data.frame (plt.lst[["nodal.status"]]),
+            add = TRUE,
+            method = "jitter",
+            pch = 19,
+            vertical = TRUE,
+            cex = 2,
+            col = transparent.rgb ("black"))
+## myBoxplot (plt.lst[["nodal.status"]],
+##            xlab = "% node positive patients",
+##            ylab = frac.label)
 ## file = "boxplot-metabric-node.svg")
 
 ## panel F
